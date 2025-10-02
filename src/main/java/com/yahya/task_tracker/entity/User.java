2382551915509f3d2c02d063be0ea3ec3864c1b0
Yahya S. Abdallah <149334812+yahyasheriif@users.noreply.github.com>
2025-10-02 08:59:10 +0300

    Add files via upload

---
+++ src/main/java/com/yahya/task_tracker/entity/User.java
@@ -0,0 +1,31 @@
+package com.yahya.task_tracker.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="_user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "userName")
+    private List<Task> tasks;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/yahya/task_tracker/service/TaskService.java
@@ -0,0 +1,64 @@
+package com.yahya.task_tracker.service;
+import com.yahya.task_tracker.entity.Task;
+import com.yahya.task_tracker.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import java.util.List;
+
+
+@Service
+public class TaskService {
+    @Autowired
+    TaskRepository taskRepository;
+
+    public List<Task> fetchAllTasks() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        List<Task> tasks = taskRepository.findByUserName(username);
+        return tasks;
+    }
+
+    public String updateTask(Long id ,Task task) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        if(!taskRepository.findById(id).orElse(null).getUserName().equals(username)) {
+            return "You are not authorized to update this task";
+        }
+        Task curTask = taskRepository.findById(id).orElse(null);
+        if(curTask==null) {
+            return "Task not found";
+        }
+            curTask.setName(task.getName());
+            curTask.setDescription(task.getDescription());
+            curTask.setStatus(task.getStatus());
+            taskRepository.save(curTask);
+            return "Task updated successfully";
+    }
+
+    public String createTask(@Validated Task task ) {
+        try {
+            task.setUserName(SecurityContextHolder.getContext().getAuthentication().getName());
+            taskRepository.save(task);
+            return "Task created successfully";
+        } catch (Exception e) {
+            return "Error occurred while creating task";
+        }
+    }
+
+    public String deleteTask(Long id) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        if(!taskRepository.findById(id).orElse(null).getUserName().equals(username)) {
+            return "You are not authorized to delete this task";
+        }
+        Task curTask = taskRepository.findById(id).orElse(null);
+        if(curTask == null) {
+            return "Task not found";
+        }
+        taskRepository.delete(curTask);
+        return "Task deleted successfully";
+    }
+}
